c967210c96a924ddd039c854d1f9a7137b76a347
Jennifer Lara <jenniferlara@Jennifers-MacBook-Pro.local>
2017-07-14 18:05:44 -0400

    fixed best friend match math

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 //import friend data
-
 var friendData = require('../data/friends.js');
 
 
@@ -15,45 +14,61 @@ module.exports = function(app) {
     // post route for /api/friends takes in the new data &
     // responds w/most compatible friend
     app.post('/api/friends', function(req, res) {
-       const userInput = req.body;
-       const userAnswers = userInput.scores;
-       
-    
-        //best friend match variables
-        const matchName = '';
-        const matchPic = '';
-        const totalDifference = 1000; //large for comparison
+        //sets up the new user's response as a variable
+        var newFriend = req.body;
 
-    
-    //run through all possible matches in friendData
-    for (var i = 0; i <friendData.length; i++) {
-        console.log('friend = ' + JSON.stringify(friendData[i]));
-
-        //compute differences foe every question
-        const difference = 0;
-        for (var x = 0; x < userAnswers.length; x++) {
-            difference += Math.abs(friendData[x].scores[x] - userAnswers[x]);
+        for (var i = 0; i < newFriend.scores.length; i++) {
+            if (newFriend.scores[i] == "1 (Strongly Disagree)") {
+                newFriend.scores[i] = 1;
+            } else if (newFriend.scores[i] == "5 (Strongly Agree)") {
+                newFriend.scores[i] = 5;
+            } else {
+                newFriend.scores[i] = parseInt(newFriend.scores[i]);
+            }
         }
-        console.log('difference = ' +difference);
 
-        //record the match for the friend with the lowest difference
-        if (difference < totalDifference) {
-            console.log('Closest match found = ' + difference);
-            console.log('Friend name = ' + friendData[i].name);
-            console.log('Friend photo = ' + friendData[i].photo);
+        //empty differences array-->this is where we will collect the differences between the current 
+        //user's scores and the scores of all other friends in friendData
+        var differencesArray = [];
 
-            totalDifference = difference;
-            matchName = friendData[i].name;
-            matchPhoto = friendData[i].photo;
+        //loop through all of the friends in the friends.js file that we stored above in
+        //friendData var
+        for (var i = 0; i < friendData.length; i++) {
+            //sets initial total difference to 0
+            var totalDifference = 0;
+            //sets a specific variable for the current friend being looped through in the above for loop
+            var compFriend = friendData[i];
+            //for loop that runs through compfriend's scores
+            for (var k = 0; k < compFriend.scores.length; k++) {
+                //sets variable of difference between compfriend's score & the current user's score
+                var differenceOneScore = Math.abs(compFriend.scores[k] - newFriend.scores[k]);
+                //adds the value of differenceOneScore to totalDifference
+                totalDifference += differenceOneScore;
+            }
+            //sets the above empty array's value to the total differences for all friendData that was
+            //looped through 
+            differencesArray[i] = totalDifference;
         }
-    }
+        // console.log(differencesArray)
+            //sets two variables: the first is for the first score in the differencesArray created above
+            //the second sets a score index of 0
+        var bestFriendNum = differencesArray[0];
+        var bestFriendIndex = 0;
 
-    //add new user
-    friendData.push(userInput);
+        //for loop to loop through the scores in the differencesArray & determine best friend match
+        for (var i = 1; i < differencesArray.length; i++) {
+            //creates if statement that will determine lowest number in differencesArray
+            if (differencesArray[i] < bestFriendNum) {
+                bestFriendNum = differencesArray[i];
+                bestFriendIndex = i;
+            }
+        }
 
-    //send response
-     res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+        //pushes the current user's data to friendData aka ../data/friends.js
+        friendData.push(newFriend);
+        // console.log(newFriend)
 
-        });
-
+        //sends the JSON response
+        res.json(friendData[bestFriendIndex]);
+    })
 }
