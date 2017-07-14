e57ef16dc57be9f819f878e3771a94db92e2ab21
Jennifer Lara <jenniferlara@Jennifers-MacBook-Pro.local>
2017-07-14 18:18:06 -0400

    added comments

--- server.js
+++ server.js
@@ -22,7 +22,7 @@
 
     app.use(express.static('public'));
 
-    //Import routes.
+    //import api routes
     require('./app/routing/apiRoutes')(app);
     require('./app/routing/htmlRoutes')(app);
 
